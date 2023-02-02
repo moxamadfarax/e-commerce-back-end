229fee82aa333c428f38c62230a3b9e044e92d83
Mohamed Farah <mohamedfarah0508@gmail.com>
2023-02-02 04:09:30 -0800

    completed request for getting category by id

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -15,7 +15,23 @@ router.get("/", async (req, res) => {
 });
 
 // Route to get a product by it's id.
-router.get("/:id", (req, res) => {});
+router.get("/:id", (req, res) => {
+  Product.findOne({
+    include: [{ model: ProductTag }, { model: Tag }, { model: Category }],
+    where: { id: req.params.id },
+  })
+    .then((product) => {
+      if (!product) {
+        return res.status(404).json({
+          message: `Product with ID ${req.params.id} not found. Please try again with another id.`,
+        });
+      }
+      res.json(product);
+    })
+    .catch((error) => {
+      res.status(500).json({ message: error.message });
+    });
+});
 
 // Route to create a new product.
 router.post("/", (req, res) => {});
