76c3c9c3e9491aa0aba3e54ad76e71ba16dbf414
Mohamed Farah <mohamedfarah0508@gmail.com>
2023-02-02 04:22:01 -0800

    completed route to update tag by id

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -34,7 +34,34 @@ router.post("/", (req, res) => {
 });
 
 // Route to update tag by it's id.
-router.put("/:id", (req, res) => {});
+router.put("/:id", (req, res) => {
+  if (req.body.tag_name === undefined) {
+    res.status(404).json({
+      message: `Could not find requested tag name. Please try again.`,
+    });
+    return;
+  }
+  Tag.update(
+    {
+      tag_name: req.body.tag_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((updatedTag) => {
+      if (updatedTag == 0) {
+        res.status(404).json({
+          message: `No records were modified with this put request for id ${req.params.id}. Please check your id, and updated name and try again!`,
+        });
+        return;
+      }
+      res.json({ message: "Tag Name has been updated" });
+    })
+    .catch((err) => res.json(err));
+});
 
 // Route to delete tag by it's id.
 router.delete("/:id", (req, res) => {});
